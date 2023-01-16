a6024f85c8fa235d931273d9a9572c6ef55b5164
Kooreis <kooreis.official@gmail.com>
2023-01-16 14:39:30 -0600

    This first part of the script defines the function minWindow and initializes a map object to count the frequency of each character in the target string. It also sets a count variable to the number of unique characters in the target string.

--- solution.ts
+++ solution.ts
@@ -1,41 +1,5 @@
-Here is a TypeScript solution for the problem:
-
-```typescript
 function minWindow(s: string, t: string): string {
     let map: any = {};
     t.split('').forEach(ch => map[ch] = (map[ch] || 0) + 1);
     let count = Object.keys(map).length;
-    let left = 0;
-    let right = 0;
-    let head = 0;
-    let len = Infinity;
-
-    while (right < s.length) {
-        let r = s[right];
-        if (map[r] !== undefined) {
-            map[r]--;
-            if (map[r] === 0) count--;
-        }
-        right++;
-
-        while (count === 0) {
-            let l = s[left];
-            if (map[l] !== undefined) {
-                map[l]++;
-                if (map[l] > 0) count++;
-            }
-            if (right - left < len) {
-                len = right - left;
-                head = left;
-            }
-            left++;
-        }
-    }
-    if (len === Infinity) return "";
-    return s.substr(head, len);
 }
-
-console.log(minWindow("ADOBECODEBANC", "ABC"));
-```
-
-This TypeScript solution uses a sliding window approach to find the minimum window substring. It first creates a map of all the characters in the target string, then it iterates over the source string, expanding the right end of the window. When all the characters in the target string are included in the window, it starts to shrink the window from the left until the window no longer contains all characters. It keeps track of the minimum length window that contains all characters. If no such window is found, it returns an empty string.
